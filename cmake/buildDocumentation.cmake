004e6ec399c99c512cffc6936f7684b4c048e281
Henry Borchers <henryborchers@yahoo.com>
2016-10-08 21:59:47 -0500

    fixed folderlocation

--- cmake/buildDocumentation.cmake
+++ cmake/buildDocumentation.cmake
@@ -1,9 +1,10 @@
 find_package(Doxygen)
 
 #set(mainpage ${CMAKE_CURRENT_SOURCE_DIR}/documentation/mainpage.md)
-#set(root ${CMAKE_CURRENT_SOURCE_DIR}/include)
+set(root ${CMAKE_CURRENT_SOURCE_DIR}/lib)
+set(DOC_OUTPUT ${CMAKE_BINARY_DIR}/documentation)
 if (DOXYGEN_FOUND)
-    configure_file(${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
+    configure_file(${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
     add_custom_target(doc ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
             WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
             COMMENT "Generating documentation" VERBATIM)
