c12b1a29418ba974a31019e6792484e43464b17c
Henry Borchers <henryborchers@yahoo.com>
2016-10-08 22:27:01 -0500

    added tests for SetVisVisualResultData

--- tests/src/TESTvisBuffer.cpp
+++ tests/src/TESTvisBuffer.cpp
@@ -49,13 +49,41 @@ TEST_F(visVisualResultFunctions, resize) {
 TEST_F(visVisualResultFunctions, zeroOnInit){
     uint8_t value = 100;
     SetVisVisualResultReadySize(result, 10);
-    GetVisVisualResultReadyValue(result, &value, 5);
+    GetVisVisualResultValue(result, &value, 5);
     ASSERT_EQ(value, 0);
 }
 
 TEST_F(visVisualResultFunctions, visVisualResultFunctions_getBadVaue_Test){
     uint8_t value = 100;
     SetVisVisualResultReadySize(result, 10);
-    ASSERT_EQ(GetVisVisualResultReadyValue(result, &value, 100), EFAULT);
+    ASSERT_EQ(GetVisVisualResultValue(result, &value, 100), EFAULT);
+
+}
+
+TEST_F(visVisualResultFunctions, setresultdata) {
+    uint8_t value = 100;
+    uint8_t foo[] = {0,1,2,3,4,5,6,7,8,9};
+    uint8_t *bar = (uint8_t*)malloc(sizeof(uint8_t) * 10);
+    bar[5] = 9;
+
+    SetVisVisualResultReadySize(result, 10);
+
+    // Test stack
+    ASSERT_FALSE(isVisVisualResultReady(result));
+    SetVisVisualResultData(result, foo, 10);
+    GetVisVisualResultValue(result, &value, 5);
+    ASSERT_EQ(value, 5);
+    GetVisVisualResultValue(result, &value, 0);
+    ASSERT_TRUE(isVisVisualResultReady(result));
+    ASSERT_EQ(value, 0);
+
+    // test heap
+    SetVisVisualResultData(result, bar, 10);
+    GetVisVisualResultValue(result, &value, 5);
+    ASSERT_EQ(value, 9);
+    bar[5] = 2;
+    free(bar);
+    bar = NULL;
+    ASSERT_EQ(value, 9);
 
 }
