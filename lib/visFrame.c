78f4365f92ab385eb6a5af0bc15983a98e32ce4d
Henry Borchers <henryborchers@yahoo.com>
2016-10-08 23:04:33 -0500

    fixed doxy documentation. fixed missing return 0

--- lib/visFrame.c
+++ lib/visFrame.c
@@ -13,15 +13,16 @@ enum PIXELCOLORSPACE {
 };
 
 /**
+ * @struct PixelYUV
  * @brief The pxiel split into it's YUV components
  *
  * This represent a pixel's value. NOTE: While YUV is really an analog color format and the true format is YCrBr,
  * it's much easier to write YUV as a varible name and YCrBr is more thought of in terms of their analog counterpart.
  */
 struct _PixelYUV {
-    uint8_t Y;    /** Luma value for a pixel*/
-    uint8_t U;    /** Cr chroma value for pixel*/
-    uint8_t V;    /** Br chroma value for pixel*/
+    uint8_t Y;    /**< Luma value for a pixel.*/
+    uint8_t U;    /**< Cr chroma value for pixel.*/
+    uint8_t V;    /**< Br chroma value for pixel.*/
 };
 
 
@@ -29,10 +30,10 @@ struct _PixelYUV {
  * @brief A frame of video. Each pixel has it's subsampling destructured so that each pixel has a luma and two chroma values.
  */
 struct _VisYUVFrame{
-    int height;                     /** Number of rows of pixels in the frame*/
-    int width;                      /** Number of columns of pixels in the frame*/
-    struct _PixelYUV ***data;       /** Pixels that make up the frame*/
-    int64_t pos;                    /** timestamp for the frame */
+    int height;                     /**< Number of rows of pixels in the frame.*/
+    int width;                      /**< Number of columns of pixels in the frame.*/
+    struct _PixelYUV ***data;       /**< Pixels that make up the frame.*/
+    int64_t pos;                    /**< timestamp for the frame.*/
 } ;
 
 void FreeFrameData(VisYUVFrame **pFrame);
@@ -71,6 +72,7 @@ int GetPixelYUV(PixelYUV *pixel, uint8_t *y, uint8_t *u, uint8_t *v) {
     *y = pixel->Y;
     *u = pixel->U;
     *v = pixel->V;
+    return 0;
 }
 
 
