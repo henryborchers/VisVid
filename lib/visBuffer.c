9cb8b4ae572038a2383a88bbbdc6bcea29dee9d5
Henry Borchers <henryborchers@yahoo.com>
2016-10-08 21:54:50 -0500

    init commit

---
+++ lib/visBuffer.c
@@ -0,0 +1,78 @@
+//
+// Created by Borchers, Henry Samuel on 10/8/16.
+//
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <errno.h>
+#include "visBuffer.h"
+
+struct _visVisualResult{
+    bool ready;
+    int size;
+    uint8_t *data;
+};
+
+visVisualResult *CreateVisVisualResult() {
+    visVisualResult *newResult = NULL;
+    newResult = (visVisualResult*)malloc(sizeof(visVisualResult));
+    newResult->ready = false;
+    newResult->size = -1;
+    newResult->data = NULL;
+    return newResult;
+}
+
+void DestroyVisVisualResult(visVisualResult **pRes) {
+    free((*pRes)->data);
+
+    (*pRes)->data = NULL;
+    (*pRes)->size = -1;
+    (*pRes)->ready = false;
+    free((*pRes));
+    *pRes = NULL;
+
+}
+
+bool isVisVisualResultReady(visVisualResult *pRes) {
+    return pRes->ready;
+}
+
+int SetVisVisualResultReadySize(visVisualResult *pRest, int size) {
+    if(pRest == NULL){
+        return EFAULT;
+    }
+    if(pRest->size > 0) {
+        free(pRest->data);
+    }
+    pRest->data = NULL;
+    pRest->data = calloc((size_t )size, sizeof(uint8_t));
+    if(pRest->data == NULL){
+        pRest->size = -1;
+        pRest->ready = false;
+        return ENOMEM;
+    }
+    pRest->size = size;
+    return 0;
+}
+
+int GetVisVisualResultReadySize(visVisualResult *pRest, int *size) {
+    if(pRest == NULL){
+        return EFAULT;
+    }
+    *size = pRest->size;
+    return 0;
+}
+
+int GetVisVisualResultReadyValue(visVisualResult *pRes, uint8_t *value, uint16_t offset) {
+    if(value == NULL){
+        return EFAULT;
+    }
+    if(offset > pRes->size){
+        return EFAULT;
+    }
+    *value = pRes->data[offset];
+    return 0;
+}
+
---
+++ lib/visBuffer.h
@@ -0,0 +1,57 @@
+//
+// Created by Borchers, Henry Samuel on 10/8/16.
+//
+/**
+ * @file visBuffer.h
+ * @author Henry Borchers
+ * @date October 8, 2016
+ * @brief Contains all logic for the buffer and the visualization caluculation results
+ */
+
+#ifndef VISVID_VISBUFFER_H
+#define VISVID_VISBUFFER_H
+typedef struct _visVisualResult visVisualResult;
+
+/**
+ * Creates a new visVisualResult on the heap and returns a pointer to it.
+ * @return Pointer to new visVisualResult.
+ */
+visVisualResult *CreateVisVisualResult();
+
+/**
+ * Frees up the memory used by a given visVisualResult.
+ * @param pRes Pointer to the visVisualResult to free up.
+ */
+void DestroyVisVisualResult(visVisualResult **pRes);
+
+/**
+ * Checks the result has valid data from a calculation
+ */
+bool isVisVisualResultReady(visVisualResult *pRes);
+
+/**
+ * Sizes or resizes the length of the visVisualResult. Any existing data will be lost.
+ * @param pRest Pointer to visVisualResult to set the length.
+ * @param size The new length to set the result to. This is most like the width or the height of the frame.
+ * @return Returns 0 on success.
+ */
+int SetVisVisualResultReadySize(visVisualResult *pRest, int size);
+
+/**
+ * Gets the size of length of a visVisualResult.
+ * @param pRest Pointer to visVisualResult to get the length.
+ * @param size A pointer to where the length can be stored.
+ * @return Returns 0 on success.
+ */
+int GetVisVisualResultReadySize(visVisualResult *pRest, int *size);
+
+/**
+ * Get the value of a visVisualResult at a certain offset.
+ * @param pRes
+ * @param value
+ * @param offset
+ * @return
+ */
+int GetVisVisualResultReadyValue(visVisualResult *pRes, uint8_t *value, uint16_t offset);
+
+#endif //VISVID_VISBUFFER_H
