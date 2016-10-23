a7ea508c04e07882b63a2019654bece6e1c41488
Henry Borchers <henryborchers@yahoo.com>
2016-10-22 20:50:54 -0500

    added test for Brush functions

--- tests/src/TESTvisFrame.cpp
+++ tests/src/TESTvisFrame.cpp
@@ -12,10 +12,10 @@ extern "C"{
 
 using namespace std;
 
-
-TEST(visFrame, version) {
-    EXPECT_EQ(VISFRAME_VERSION, VISFRAME_VERSION);
-}
+//
+//TEST(visFrame, version) {
+//    EXPECT_EQ(VISFRAME_VERSION, VISFRAME_VERSION);
+//}
 
 struct visPixelYUVFunctions : public ::testing::Test{
     PixelYUV *pixel;
@@ -64,15 +64,19 @@ TEST(visPixelYUVSetup, visPixelYUV_CreateAndCleanup){
 }
 
 TEST_F(visPixelYUVFunctions, setPixel){
-    ASSERT_EQ(SetPixelYUV(pixel, 4 , 100, 40), 0);
+    ASSERT_EQ(SetPixelValueYUV(pixel, 4, 100, 40), 0);
+}
+TEST_F(visPixelYUVFunctions, setPixelBAD){
+    PixelYUV *badpix = NULL;
+    ASSERT_EQ(SetPixelValueYUV(badpix, 4, 100, 40), EFAULT);
 }
 
 TEST_F(visPixelYUVFunctions, setAndGetPixel){
     PixelValue y = 0;
     PixelValue u = 0;
     PixelValue v = 0;
-    SetPixelYUV(pixel, 4 , 100, 40);
-    GetPixelYUV(pixel, &y, &u, &v);
+    SetPixelValueYUV(pixel, 4, 100, 40);
+    GetPixelValueYUV(pixel, &y, &u, &v);
 
     ASSERT_EQ(y, 4);
     ASSERT_EQ(u, 100);
@@ -112,4 +116,58 @@ TEST_F(visFrameYUVFunctions, setFrameSize){
     GetVisYUVFrameSize(foo, &width, &height);
     ASSERT_EQ(height, 480);
     ASSERT_EQ(width, 640);
+}
+
+TEST_F(visFrameYUVFunctions, BrushFill) {
+    int height = 100;
+    int width = 200;
+    visBrush brush;
+    brush.Y = 20;
+    brush.U = 30;
+    brush.V = 40;
+    PixelYUV pix;
+
+    SetVisYUVFrameSize(foo, width, height);
+    ASSERT_EQ(visFillYUVFrame(foo, &brush), 0);
+    ASSERT_EQ(GetPixelFromYUVFrame(&pix, foo, 50, 50), 0);
+
+    PixelValue y = 0;
+    PixelValue u = 0;
+    PixelValue v = 0;
+    GetPixelValueYUV(&pix, &y, &u, &v);
+    ASSERT_EQ(y, brush.Y);
+    ASSERT_EQ(u, brush.U);
+    ASSERT_EQ(v, brush.V);
+
+}
+
+TEST_F(visFrameYUVFunctions, BrushDraw) {
+    int height = 20;
+    int width = 100;
+    visBrush brush;
+    brush.Y = 20;
+    brush.U = 30;
+    brush.V = 40;
+    PixelYUV pix;
+
+
+    SetVisYUVFrameSize(foo, width, height);
+    for(PixelValue x = 0; x < width; x++){
+        brush.Y = x;
+        visDrawYUVPixel(foo, &brush, x, 0);
+    }
+
+    for(PixelValue x = 0; x < width; x++){
+        ASSERT_EQ(GetPixelFromYUVFrame(&pix, foo, x, 0), 0);
+        PixelValue y = x;
+        PixelValue u = 0;
+        PixelValue v = 0;
+        GetPixelValueYUV(&pix, &y, &u, &v);
+        ASSERT_EQ(y, x);
+        ASSERT_EQ(u, brush.U);
+        ASSERT_EQ(v, brush.V);
+    }
+
+
+
 }
