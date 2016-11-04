1c9aa4a780f8d3eea42f599571c482d5f6dfe24a
Henry Borchers <henryborchers@yahoo.com>
2016-11-03 21:20:53 -0500

    saves documentation to CMAKE_BINARY_DIR now instead of CMAKE_ARCHIVE_OUTPUT_DIRECTORY

--- cmake/buildDocumentation.cmake
+++ cmake/buildDocumentation.cmake
@@ -1,7 +1,7 @@
 find_package(Doxygen)
 
 set(root ${PROJECT_SOURCE_DIR}/lib)
-set(DOC_OUTPUT ${CMAKE_ARCHIVE_OUTPUT_DIRECTORY}/documentation)
+set(DOC_OUTPUT ${CMAKE_BINARY_DIR}/documentation)
 if (DOXYGEN_FOUND)
     configure_file(${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
     add_custom_target(doc ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
