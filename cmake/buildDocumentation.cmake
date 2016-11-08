d6a5c5dd89adcac01f8ba4e42d9c64cdf789cefa
Henry Borchers <henryborchers@yahoo.com>
2016-11-08 08:30:40 -0600

    Made Doxygen optional

--- cmake/buildDocumentation.cmake
+++ cmake/buildDocumentation.cmake
@@ -2,11 +2,14 @@ find_package(Doxygen)
 
 set(root ${PROJECT_SOURCE_DIR}/lib)
 set(DOC_OUTPUT ${CMAKE_BINARY_DIR}/documentation)
-if (DOXYGEN_FOUND)
-    configure_file(${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
-    add_custom_target(doc ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
-            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-            COMMENT "Generating documentation" VERBATIM)
+
+if(VISVID_BUILDDOCS)
+    if (DOXYGEN_FOUND)
+        configure_file(${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
+        add_custom_target(doc ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
+                WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+                COMMENT "Generating documentation" VERBATIM)
 
 
-endif ()
+    endif(DOXYGEN_FOUND)
+endif(VISVID_BUILDDOCS)
