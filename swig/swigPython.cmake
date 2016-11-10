ee002d4c321a12e901335a7c83cfb6690c15ad92
Henry Borchers <henryborchers@yahoo.com>
2016-11-10 00:52:11 -0500

    requires python 3 now

--- swig/swigPython.cmake
+++ swig/swigPython.cmake
@@ -1,5 +1,5 @@
 find_package(SWIG REQUIRED)
-FIND_PACKAGE(PythonLibs)
+FIND_PACKAGE(PythonLibs 3)
 if(VISVID_BuildPython3Bindings)
     message(STATUS "Building Python SWIG Bindings")
 
@@ -10,10 +10,23 @@ if(VISVID_BuildPython3Bindings)
 
     INCLUDE_DIRECTORIES(${CMAKE_SOURCE_DIR}/lib)
 
+        # FIXME: REMOVE THIS BOTTOM TO DYNAMICALLY FIND NUMPY
+    INCLUDE_DIRECTORIES(/usr/local/lib/python3.5/site-packages/numpy/core/include)
+
+
     SET(CMAKE_SWIG_FLAGS "-py3")
-    SWIG_ADD_MODULE(visVid python swig/visVid.i lib/visFrame.c lib/visBuffer.c lib/visResult.c
-        lib/visView.c lib/visView.h
-        lib/visVisualization.c lib/visVisualization.h
-        lib/visTypes.h)
+    SWIG_ADD_MODULE(
+            visVid
+            python
+            swig/visVid.i
+            lib/visFrame.c
+            lib/visBuffer.c
+            lib/visResult.c
+            lib/visView.c
+            lib/visView.h
+            lib/visVisualization.c
+            lib/visVisualization.h
+            lib/visTypes.h)
     SWIG_LINK_LIBRARIES(visVid ${PYTHON_LIBRARIES})
+
 endif()
