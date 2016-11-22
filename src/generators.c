398b50976d369f9e2fc12557a35eca878575c801
Henry Borchers <henryborchers@yahoo.com>
2016-11-22 15:18:34 -0600

    init commit

---
+++ src/generators.c
@@ -0,0 +1,21 @@
+//
+// Created by Borchers, Henry Samuel on 11/22/16.
+//
+
+#include "generators.h"
+
+int ramp(visImageRGB *t) {
+    static uint8_t count = 0;
+
+    for(int y = 0; y < t->height; y++){
+        for(int x = 0; x < t->width; x++){
+            visImageWritePixelRGB(t, x, y, 0, count, (uint8_t)(((float)x /t->width - count) * 255), 255);
+//             write_pixel(t, y, x, 1, 100, 2, (uint32_t)(((float)x /t->width - count) * 255));
+        }
+    }
+
+    count++;
+
+    return 0;
+    return 0;
+}
---
+++ src/generators.h
@@ -0,0 +1,11 @@
+//
+// Created by Borchers, Henry Samuel on 11/22/16.
+//
+
+#ifndef VISVID_DUMMY_H
+#define VISVID_DUMMY_H
+
+#include "visImageWriter.h"
+
+int ramp(visImageRGB *t);
+#endif //VISVID_DUMMY_H
