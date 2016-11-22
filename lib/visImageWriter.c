6e223e435ce6cb800bb492e88cb31d55e750bbac
Henry Borchers <henryborchers@yahoo.com>
2016-11-22 13:33:22 -0600

    fixed bug where the memory wasn't correctly allocating because the pitch hadn't been calculated yet.

--- lib/visImageWriter.c
+++ lib/visImageWriter.c
@@ -7,29 +7,33 @@
 #include <errno.h>
 #include "visImageWriter.h"
 
-int visAllocImageRGB(VisImageRGB *t, int height, int width) {
-    t->plane = malloc(sizeof(uint8_t) * t->pitch * height * 4);
+int visAllocImageRGB(visImageRGB *t, int width, int height) {
+    int pitch = width * 4;
+    t->plane = malloc(sizeof(uint8_t) * pitch * height * 4);
     if(NULL == t->plane){
         return ENOMEM;
     }
+    t->pitch = pitch;
 
     t->height = height;
     t->width = width;
 
-    t->pitch = width * 4;
     return 0;
 }
 
-void visFreeImageRGB(VisImageRGB *t) {
+void visFreeImageRGB(visImageRGB *t) {
     if(t->plane != NULL){
         free(t->plane);
-        t->plane= NULL;
+        t->plane = NULL;
     }
+    t->width = -1;
+    t->height = -1;
+    t->pitch = -1;
 
 
 }
 
-int writePixelRGB(VisImageRGB *t, int pixelY, int pixelX, uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
+int visImageWritePixelRGB(visImageRGB *t, int pixelX, int pixelY, uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
     int y_offset = pixelY * t->pitch;
     int x_offset = (pixelX * sizeof(uint8_t) * 4);
     int offset = y_offset + x_offset;
--- lib/visImageWriter.h
+++ lib/visImageWriter.h
@@ -11,17 +11,19 @@
 
 #include <stdint.h>
 
+typedef struct visImageRGB visImageRGB;
 
 /**
  * @struct VisImageRGB
  * @brief RGB image data
  */
-typedef struct {
+struct visImageRGB{
+//typedef struct {
     uint8_t *plane;     /**< Raw data for pixel information.*/
     int     pitch;      /**< Pitch of the image.*/
     int     height;     /**< Height of the image.*/
     int     width;      /**< Width of the image.*/
-}VisImageRGB;
+};
 
 /**
  * Allocates the amount of memory needed to store a the data for an RGBA8888 image. You are responsible for freeing
@@ -31,7 +33,7 @@ typedef struct {
  * @param width The number of pixels wide for the desired image.
  * @return 0 Returns zero on success. Anything else is an error.
  */
-int visAllocImageRGB(VisImageRGB *t, int height, int width);
+int visAllocImageRGB(visImageRGB *t, int width, int height);
 
 /**
  *
@@ -45,12 +47,12 @@ int visAllocImageRGB(VisImageRGB *t, int height, int width);
  * @param a The amount of assigned to the alpha channel to be used in the given pixel.
  * @return Returns zero on success.
  */
- int writePixelRGB(VisImageRGB *t, int pixelY, int pixelX, uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+int visImageWritePixelRGB(visImageRGB *t, int pixelX, int pixelY, uint8_t r, uint8_t g, uint8_t b, uint8_t a);
 
 /**
  * Frees up the data being used by a VisImageRGB object
  * @param t VisImageRGB to write pixel data to
  */
-void visFreeImageRGB(VisImageRGB *t);
+void visFreeImageRGB(visImageRGB *t);
 
 #endif //VISVID_VISIMAGES_H
