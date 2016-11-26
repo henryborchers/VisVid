fd3408b2abc251f3ff562e05b947bdcf1b7e0682
Henry Borchers <henryborchers@yahoo.com>
2016-11-25 21:47:02 -0600

    changed the program from running the visual generator to decoding a video image.

--- src/createVisual.c
+++ src/createVisual.c
@@ -4,19 +4,57 @@
 
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <libavutil/frame.h>
 #include "gui.h"
 #include "generators.h"
 #include "visVersion.h"
+#include "videoDecoder.h"
 
 #define SCREENWIDTH 640
 #define SCREENHEIGHT 480
 
 
+int generate_waves();
+
 int main(int argc, char *argv[]){
+
+    DecoderContext      *decoderCtx = NULL;
+    AVFrame             *frame = NULL;
+    decoder_init();
 
     puts("Using VisVis library");
     printf("Version %s\n", VERSION_VIS_S);
+    fflush(stdout);
 
+    // Check args
+    if(argc != 2){
+        fprintf(stderr, "You need a file name to continue\n");
+        return 1;
+    }
+
+
+    puts("Opening video");
+    decoderCtx = decoderContext_Create(argv[1]);
+    if(decoderCtx == NULL){
+        fprintf(stderr, "Unable to load file \"%s\"", argv[1]);
+        return 1;
+    } else{
+        puts("Video Opened");
+    }
+
+    // loop over video until hitting an error or the end of the file
+    while(decoderContext_NextFrame(decoderCtx, &frame) >= 0){
+
+        // TODO: Render out frame
+
+    };
+
+    puts("Closing video ");
+    decoderContext_Destroy(&decoderCtx);
+
+}
+
+int generate_waves() {
     gui_context gui;
     SDL_Event event;
 //    SDL_Rect r;
@@ -54,5 +92,5 @@ int main(int argc, char *argv[]){
     }
     puts("Deleting RGBA Image");
     gui_destroy_window(&gui);
-
+    return 0;
 }
