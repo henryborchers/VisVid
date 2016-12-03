e1293ffff51650a4ca01094a5e9d0813ce7c3d01
Henry Borchers <henryborchers@yahoo.com>
2016-12-03 14:46:51 -0600

    refactored player into own source file

--- src/createVisual.c
+++ src/createVisual.c
@@ -3,24 +3,16 @@
 //
 
 #include <stdio.h>
-#include <SDL2/SDL.h>
-#include <libavutil/frame.h>
-#include "gui.h"
-#include "generators.h"
 #include "visVersion.h"
-#include "videoDecoder.h"
-
-#define SCREENWIDTH 640
-#define SCREENHEIGHT 480
+#include "runPlayers.h"
 
 
-int generate_waves();
+
 
 int main(int argc, char *argv[]){
 
-    DecoderContext      *decoderCtx = NULL;
-    AVFrame             *frame = NULL;
-    decoder_init();
+    int ret;
+
 
     puts("Using VisVis library");
     printf("Version %s\n", VERSION_VIS_S);
@@ -32,65 +24,13 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
-
-    puts("Opening video");
-    decoderCtx = decoderContext_Create(argv[1]);
-    if(decoderCtx == NULL){
-        fprintf(stderr, "Unable to load file \"%s\"", argv[1]);
-        return 1;
-    } else{
-        puts("Video Opened");
-    }
-
-    // loop over video until hitting an error or the end of the file
-    while(decoderContext_NextFrame(decoderCtx, &frame) >= 0){
-
-        // TODO: Render out frame
-
+    if((ret = playVideoSimple(argv[1])) !=0){
+        fprintf(stderr, "program didn't run correctly\n");
     };
 
-    puts("Closing video ");
-    decoderContext_Destroy(&decoderCtx);
+
 
 }
 
-int generate_waves() {
-    gui_context gui;
-    SDL_Event event;
-//    SDL_Rect r;
-    visImageRGB t;
-
-//    r.h = 100;
-//    r.w = 100;
-//    r.x = 10;
-//    r.y = 10;
 
 
-    puts("Hello");
-    puts("Creating RGBA Image");
-
-    visImageRGB_Alloc(&t, SCREENWIDTH, SCREENHEIGHT);
-
-    if(gui_init() != 0){
-        return 1;
-    }
-
-    gui_ctx_init(&gui);
-
-    gui.windowWidth = SCREENWIDTH;
-    gui.windowHeight = SCREENHEIGHT;
-    gui_build_window(&gui);
-
-
-    while(1){
-        SDL_PollEvent(&event);
-        if(event.type == SDL_QUIT){
-            break;
-        }
-        ramp(&t);
-        gui_refresh(&gui, &t);
-    }
-    puts("Deleting RGBA Image");
-    gui_destroy_window(&gui);
-    return 0;
-}
