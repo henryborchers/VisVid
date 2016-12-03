f8d57577d532f8210a5fdbc14888edc4207c0c29
Henry Borchers <henryborchers@yahoo.com>
2016-12-03 14:50:45 -0600

    init commit.

---
+++ src/runPlayers.h
@@ -0,0 +1,12 @@
+//
+// Created by Borchers, Henry Samuel on 12/3/16.
+//
+/***
+ * You should only have to import file header to gain access to everything
+ */
+#ifndef LIBVISVID_RUNPLAYERS_H
+#define LIBVISVID_RUNPLAYERS_H
+
+#include "generator.h"
+#include "simpleVideoPlayer.h"
+#endif //LIBVISVID_RUNPLAYERS_H
