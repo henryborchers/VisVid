d8f9e538e03a4927207f8412ba70053d41ab2ea9
Henry Borchers <henryborchers@yahoo.com>
2016-12-06 15:09:56 -0600

    should let you now correctly get the file types in Linux OS and Windows

--- src/filedialog.c
+++ src/filedialog.c
@@ -6,7 +6,7 @@
 #include "noc/noc_file_dialog.h"
 const char *getFilename() {
 #ifdef _WIN32
-    noc_file_dialog_open(NOC_FILE_DIALOG_OPEN, "Video Files\0*.mov;*.mp4;*.mpg;*.mkv\0All Files\0*.*\0", NULL, NULL);
+    return noc_file_dialog_open(NOC_FILE_DIALOG_OPEN, "Video Files\0*.mov;*.mp4;*.mpg;*.mkv\0All Files\0*.*\0", NULL, NULL);
 #else
     return noc_file_dialog_open(NOC_FILE_DIALOG_OPEN, "Video Files\0*.mov\0Matroska\0*.mkv\0MPEG\0*.mpg\0MPEG-4\0*.mp4\0", NULL, NULL);
 #endif
