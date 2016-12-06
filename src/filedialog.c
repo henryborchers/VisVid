08966ba3cb4c5faefa8b5b2f54a432f808d80ebf
Henry Borchers <henryborchers@yahoo.com>
2016-12-06 13:48:45 -0600

    init comit

---
+++ src/filedialog.c
@@ -0,0 +1,15 @@
+//
+// Created by Borchers, Henry Samuel on 12/6/16.
+//
+
+#include "filedialog.h"
+#include "noc/noc_file_dialog.h"
+const char *getFilename() {
+#ifdef __APPLE__
+    return noc_file_dialog_open(NOC_FILE_DIALOG_OPEN, "Video Files\0*.mov\0Matroska\0*.mkv\0MPEG\0*.mpg\0MPEG-4\0*.mp4\0", NULL, NULL);
+#else
+    noc_file_dialog_open(NOC_FILE_DIALOG_OPEN, "Video Files\0*.mov;*.mp4;*.mpg;*.mkv\0All Files\0*.*\0", NULL, NULL);
+#endif
+
+
+}
---
+++ src/filedialog.h
@@ -0,0 +1,9 @@
+//
+// Created by Borchers, Henry Samuel on 12/6/16.
+//
+
+#ifndef LIBVISVID_FILEDIALOG_H
+#define LIBVISVID_FILEDIALOG_H
+
+const char *getFilename();
+#endif //LIBVISVID_FILEDIALOG_H
