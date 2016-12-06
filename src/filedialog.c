bff023377749a68fe134dad78519184e634b5d7a
Henry Borchers <henryborchers@yahoo.com>
2016-12-06 15:21:04 -0600

    renamed

--- src/filedialog.c
+++ src/filedialog.c
@@ -5,10 +5,10 @@
 #include "filedialog.h"
 #include "noc/noc_file_dialog.h"
 const char *getFilename() {
-#ifdef _WIN32
-    return noc_file_dialog_open(NOC_FILE_DIALOG_OPEN, "Video Files\0*.mov;*.mp4;*.mpg;*.mkv\0All Files\0*.*\0", NULL, NULL);
+#ifdef __APPLE__
+    return noc_file_dialog_open(NOC_FILE_DIALOG_OPEN, "Quicktime\0*.mov\0Matroska\0*.mkv\0MPEG\0*.mpg\0MPEG-4\0*.mp4\0", NULL, NULL);
 #else
-    return noc_file_dialog_open(NOC_FILE_DIALOG_OPEN, "Video Files\0*.mov\0Matroska\0*.mkv\0MPEG\0*.mpg\0MPEG-4\0*.mp4\0", NULL, NULL);
+    return noc_file_dialog_open(NOC_FILE_DIALOG_OPEN, "Video Files\0*.mov;*.mp4;*.mpg;*.mkv\0All Files\0*.*\0", NULL, NULL);
 #endif
 
 
