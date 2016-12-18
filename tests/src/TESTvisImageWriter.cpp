8450d9de19a6126dfbff05775eac9e29e313491f
Henry Borchers <henryborchers@yahoo.com>
2016-12-18 01:14:16 -0600

    added test for Write And Read Pixels

--- tests/src/TESTvisImageWriter.cpp
+++ tests/src/TESTvisImageWriter.cpp
@@ -98,7 +98,7 @@ TEST_F(visImageWriterFunctions, write_ramp_image) {
 
     for (int x = 0; x < imageRGB->width; ++x) {
         for (int y = 0; y < imageRGB->height; ++y) {
-            visImageRGB_WritePixel(imageRGB, x, y, 0, 0, 0, x);
+            visImageRGB_WritePixel(imageRGB, x, y, 0, 0, 0, (uint8_t)x);
         }
     }
 
@@ -112,5 +112,159 @@ TEST_F(visImageWriterFunctions, write_ramp_image) {
         }
     }
 
+
+}
+
+TEST(visImageRGB_WriteAndReadPixel, blackbox) {
+    const int       WIDTH  = 100;
+    const int       HEIGHT = 100;
+
+    visImageRGB     image;
+
+    ASSERT_EQ(visImageRGB_Alloc(&image, WIDTH, HEIGHT), 0);
+    for (int x = 0; x < WIDTH; ++x) {
+        for (int y = 0; y < HEIGHT; ++y) {
+            uint8_t assigned_red = 0;
+            uint8_t returned_red = 10;
+            uint8_t assigned_blue = 0;
+            uint8_t returned_blue = 10;
+            uint8_t assigned_green = 0;
+            uint8_t returned_green = 10;
+            uint8_t assigned_alpha = 0;
+            uint8_t returned_alpha = 10;
+
+            ASSERT_EQ(visImageRGB_WritePixel(&image, x, y, assigned_red, assigned_green,assigned_blue, assigned_alpha), 0);
+            ASSERT_EQ(visImageRGB_readPixel(&image, x, y, &returned_red, &returned_green, &returned_blue, &returned_alpha), 0);
+
+            ASSERT_EQ(assigned_red, returned_red);
+            ASSERT_EQ(assigned_blue, returned_blue);
+            ASSERT_EQ(assigned_green, returned_green);
+            ASSERT_EQ(assigned_alpha, returned_alpha);
+
+        }
+    }
+
+}
+
+
+TEST(visImageRGB_WriteAndReadPixel, vertical_ramp) {
+    const int       WIDTH  = 100;
+    const int       HEIGHT = 100;
+
+    visImageRGB     image;
+
+    ASSERT_EQ(visImageRGB_Alloc(&image, WIDTH, HEIGHT), 0);
+    for (int x = 0; x < WIDTH; ++x) {
+        for (int y = 0; y < HEIGHT; ++y) {
+            uint8_t assigned_red = y;
+            uint8_t returned_red = 10;
+            uint8_t assigned_blue = y;
+            uint8_t returned_blue = 10;
+            uint8_t assigned_green = y;
+            uint8_t returned_green = 10;
+            uint8_t assigned_alpha = y;
+            uint8_t returned_alpha = 10;
+
+            ASSERT_EQ(visImageRGB_WritePixel(&image, x, y, assigned_red, assigned_green,assigned_blue, assigned_alpha), 0);
+            ASSERT_EQ(visImageRGB_readPixel(&image, x, y, &returned_red, &returned_green, &returned_blue, &returned_alpha), 0);
+
+            ASSERT_EQ(assigned_red, returned_red);
+            ASSERT_EQ(assigned_blue, returned_blue);
+            ASSERT_EQ(assigned_green, returned_green);
+            ASSERT_EQ(assigned_alpha, returned_alpha);
+
+        }
+    }
+
+}
+
+TEST(visImageRGB_WriteAndReadPixel, horizontal_ramp) {
+    const int       WIDTH = 100;
+    const int       HEIGHT = 100;
+
+    visImageRGB     image;
+
+    ASSERT_EQ(visImageRGB_Alloc(&image, WIDTH, HEIGHT), 0);
+    for (int x = 0; x < WIDTH; ++x) {
+        for (int y = 0; y < HEIGHT; ++y) {
+            uint8_t assigned_red = x;
+            uint8_t returned_red = 10;
+            uint8_t assigned_blue = x;
+            uint8_t returned_blue = 10;
+            uint8_t assigned_green = x;
+            uint8_t returned_green = 10;
+            uint8_t assigned_alpha = x;
+            uint8_t returned_alpha = 10;
+
+            ASSERT_EQ(visImageRGB_WritePixel(&image, x, y, assigned_red, assigned_green,assigned_blue, assigned_alpha), 0);
+            ASSERT_EQ(visImageRGB_readPixel(&image, x, y, &returned_red, &returned_green, &returned_blue, &returned_alpha), 0);
+
+            ASSERT_EQ(assigned_red, returned_red);
+            ASSERT_EQ(assigned_blue, returned_blue);
+            ASSERT_EQ(assigned_green, returned_green);
+            ASSERT_EQ(assigned_alpha, returned_alpha);
+
+        }
+    }
+
+}
+
+TEST(visImageRGB_WriteAndReadPixel, topBottom) {
+    const int       WIDTH = 100;
+    const int       HEIGHT = 100;
+
+    visImageRGB     image;
+
+    uint8_t assigned_red = 0;
+    uint8_t returned_red = 0;
+    uint8_t assigned_blue = 0;
+    uint8_t returned_blue = 0;
+    uint8_t assigned_green = 0;
+    uint8_t returned_green = 0;
+    uint8_t assigned_alpha = 0;
+    uint8_t returned_alpha = 0;
+
+    ASSERT_EQ(visImageRGB_Alloc(&image, WIDTH, HEIGHT), 0);
+    for (int x = 0; x < WIDTH; ++x) {
+        for (int y = 0; y < HEIGHT; ++y) {
+            if(y >= 50){
+                assigned_red    = 254;
+                assigned_blue   = 254;
+                assigned_green  = 254;
+                assigned_alpha  = 254;
+
+            } else {
+                assigned_red    = 0;
+                assigned_blue   = 0;
+                assigned_green  = 0;
+                assigned_alpha  = 0;
+            }
+
+
+            ASSERT_EQ(visImageRGB_WritePixel(&image, x, y, assigned_red, assigned_green,assigned_blue, assigned_alpha), 0);
+
+        }
+    }
+
+    for (int x = 0; x < image.width; ++x) {
+        for (int y = 0; y < image.height; ++y) {
+            ASSERT_EQ(visImageRGB_readPixel(&image, x, y, &returned_red, &returned_green, &returned_blue, &returned_alpha), 0);
+
+            if(y >= 50){
+                ASSERT_EQ(254, returned_red);
+                ASSERT_EQ(254, returned_blue);
+                ASSERT_EQ(254, returned_green);
+                ASSERT_EQ(254, returned_alpha);
+
+            } else {
+
+                ASSERT_EQ(0, returned_red);
+                ASSERT_EQ(0, returned_blue);
+                ASSERT_EQ(0, returned_green);
+                ASSERT_EQ(0, returned_alpha);
+            }
+
+        }
+    }
 
 }
