73f46a7274d4879c927b62515dc6aa07dcab117f
Henry Borchers <henryborchers@yahoo.com>
2016-12-18 01:10:23 -0600

    refactored

--- src/filedialog.c
+++ src/filedialog.c
@@ -4,12 +4,14 @@
 
 #include "filedialog.h"
 #include "noc/noc_file_dialog.h"
-const char *getFilename() {
+
 #ifdef __APPLE__
-    return noc_file_dialog_open(NOC_FILE_DIALOG_OPEN, "Quicktime\0*.mov\0Matroska\0*.mkv\0MPEG\0*.mpg\0MPEG-4\0*.mp4\0", NULL, NULL);
+#define FILTER_VIDEO_FILES "Quicktime\0*.mov\0Matroska\0*.mkv\0MPEG\0*.mpg\0MPEG-4\0*.mp4\0"
 #else
-    return noc_file_dialog_open(NOC_FILE_DIALOG_OPEN, "Video Files\0*.mov;*.mp4;*.mpg;*.mkv\0All Files\0*.*\0", NULL, NULL);
+#define FILTER_VIDEO_FILES "Video Files\\0*.mov;*.mp4;*.mpg;*.mkv\\0All Files\\0*.*\\0"
 #endif
 
+const char *getFilename() {
+    return noc_file_dialog_open(NOC_FILE_DIALOG_OPEN, FILTER_VIDEO_FILES, NULL, NULL);
 
 }
