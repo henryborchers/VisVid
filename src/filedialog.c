298e4cc90664d8671ea93453957fe6de04d175d5
Henry Borchers <henryborchers@yahoo.com>
2017-01-07 14:51:52 -0600

    Simplified the filename so it's just a single search filter.

--- src/filedialog.c
+++ src/filedialog.c
@@ -5,11 +5,7 @@
 #include "filedialog.h"
 #include "noc/noc_file_dialog.h"
 
-#ifdef __APPLE__
-#define FILTER_VIDEO_FILES "Quicktime\0*.mov\0Matroska\0*.mkv\0MPEG\0*.mpg\0MPEG-4\0*.mp4\0"
-#else
-#define FILTER_VIDEO_FILES "Video Files\\0*.mov;*.mp4;*.mpg;*.mkv\\0All Files\\0*.*\\0"
-#endif
+#define FILTER_VIDEO_FILES "Video Files\0*.mov;*.mp4;*.mpg;*.mkv;*.avi\0"
 
 const char *getFilename() {
     return noc_file_dialog_open(NOC_FILE_DIALOG_OPEN, FILTER_VIDEO_FILES, NULL, NULL);
