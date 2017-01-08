8f167a0f73af900f46a8d6ba94fe8d9f2f74349e
Henry Borchers <henryborchers@yahoo.com>
2017-01-07 23:28:42 -0600

    moved deprecated macro from here to a visutil header

--- lib/visvid.h
+++ lib/visvid.h
@@ -4,14 +4,7 @@
 
 #ifndef LIBVISVID_VISVID_H
 #define LIBVISVID_VISVID_H
-#ifdef __GNUC__
-#define DEPRECATED(func) func __attribute__ ((deprecated))
-#elif defined(_MSC_VER)
-#define DEPRECATED(func) __declspec(deprecated) func
-	#else
-	#pragma message("WARNING: You need to implement DEPRECATED for this compiler")
-	#define DEPRECATED(func) func
-#endif
+
 
 #include "visFrame.h"
 #include "visBuffer.h"
