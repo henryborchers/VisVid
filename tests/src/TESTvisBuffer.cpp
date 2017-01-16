f55439e29b9ac94a7fe3534d4bbec4c8270a197c
Henry Borchers <henryborchers@yahoo.com>
2017-01-15 22:15:50 -0600

    added tests for  visBuffer_ShiftLeft and a limited buffer

--- tests/src/TESTvisBuffer.cpp
+++ tests/src/TESTvisBuffer.cpp
@@ -32,7 +32,6 @@ protected:
     }
 };
 
-
 TEST_F(visBufferFunctions, BufferIsEmptyOnCreation) {
     ASSERT_TRUE(visBuffer_isEmpty(buffer));
 
@@ -84,7 +83,60 @@ TEST_F(visBufferFunctions, get_node_position){
     ASSERT_EQ(_nodePosition(node), 0);
 }
 
-TEST_F(visBufferFunctions, visBufferFunctions_pop_and_shift_Test){
+TEST_F(visBufferFunctions, shift_Test) {
+    PixelValue          first_data_set[10]  = {1,1,1,1,1,1,1,1,1,1};
+    PixelValue          second_data_set[10] = {2,2,2,2,2,2,2,2,2,2};
+    PixelValue          third_data_set[10]  = {3,3,3,3,3,3,3,3,3,3};
+    visVisualResult     first_res;
+    visVisualResult     second_res;
+    visVisualResult     third_res;
+    visVisualResult     *current = NULL;
+
+//    Create 3 results
+    VisVisualResult_Init(&first_res);
+    VisVisualResult_SetSize(&first_res, 10);
+    VisVisualResult_SetData(&first_res, first_data_set, 10);
+    visBuffer_PushBackResult(buffer, &first_res);
+
+    VisVisualResult_Init(&second_res);
+    VisVisualResult_SetSize(&second_res, 10);
+    VisVisualResult_SetData(&second_res, second_data_set, 10);
+    visBuffer_PushBackResult(buffer, &second_res);
+
+    VisVisualResult_Init(&third_res);
+    VisVisualResult_SetSize(&third_res, 10);
+    VisVisualResult_SetData(&third_res, third_data_set, 10);
+    visBuffer_PushBackResult(buffer, &third_res);
+
+    visBuffer_ShiftLeft(buffer);
+
+    current = visBuffer_PopResult(buffer);
+    for(int i =0 ; i < second_res.size; i++){
+        PixelValue actual;
+        PixelValue expected = second_res.data[i];
+        ASSERT_EQ(VisVisualResult_GetValue(&actual, current, i), 0);
+        ASSERT_EQ(actual, expected) << "Expected: " << (int)expected << ", got " << (int)actual << ".";
+    }
+
+    current = visBuffer_PopResult(buffer);
+    for(int i =0 ; i < third_res.size; i++){
+        PixelValue actual;
+        PixelValue expected = third_res.data[i];
+        ASSERT_EQ(VisVisualResult_GetValue(&actual, current, i), 0);
+        ASSERT_EQ(actual, expected) << "Expected: " << (int)expected << ", got " << (int)actual << ".";
+    }
+
+    current = visBuffer_PopResult(buffer);
+    for(int i =0 ; i < first_res.size; i++){
+        PixelValue actual;
+        PixelValue expected = first_res.data[i];
+        ASSERT_EQ(VisVisualResult_GetValue(&actual, current, i), 0);
+        ASSERT_EQ(actual, expected) << "Expected: " << (int)expected << ", got " << (int)actual << ".";
+    }
+
+}
+
+TEST_F(visBufferFunctions, pop_and_shift_Test){
     PixelValue              data_set1[] = {1,2,3,4,5,6,7,8,9,10};
     PixelValue              data_set2[] = {10,9,8,7,6,5,4,3,2,1};
     visVisualResult         first_res;
@@ -168,3 +220,64 @@ TEST_F(visBufferFunctions, visBufferFunctions_pop_and_shift_Test){
     ASSERT_TRUE(NULL == current);
 
 }
+
+
+class visBufferLimited : public ::testing::Test {
+
+protected:
+    visBuffer *buffer;
+    virtual void SetUp() {
+        buffer = VisBuffer_Create2(10, 5);
+    }
+
+    virtual void TearDown() {
+        VisBuffer_Destroy(&buffer);
+    }
+};
+
+TEST_F(visBufferLimited, BufferCreated){
+    ASSERT_TRUE(NULL != buffer);
+
+}
+
+TEST_F(visBufferLimited, init_size){
+    ASSERT_EQ(visBuffer_getLength(buffer), 5);
+}
+
+TEST_F(visBufferLimited, push_data){
+    PixelValue first[10]  = {1,1,1,1,1,1,1,1,1,1};
+    PixelValue second[10] = {2,2,2,2,2,2,2,2,2,2};
+    PixelValue third[10]  = {3,3,3,3,3,3,3,3,3,3};
+    PixelValue fourth[10] = {4,4,4,4,4,4,4,4,4,4};
+    PixelValue fifth[10]  = {5,5,5,5,5,5,5,5,5,5};
+    PixelValue six[10]    = {6,6,6,6,6,6,6,6,6,6};
+
+    visVisualResult res;
+    VisVisualResult_Init(&res);
+    VisVisualResult_SetSize(&res, 10);
+
+    VisVisualResult_SetData(&res, first,10);
+    ASSERT_EQ(visBuffer_PushBackResult(buffer, &res), 0);
+    ASSERT_EQ(visBuffer_getLength(buffer), 5);
+
+    VisVisualResult_SetData(&res, second,10);
+    ASSERT_EQ(visBuffer_PushBackResult(buffer, &res), 0);
+    ASSERT_EQ(visBuffer_getLength(buffer), 5);
+
+    VisVisualResult_SetData(&res, third,10);
+    ASSERT_EQ(visBuffer_PushBackResult(buffer, &res), 0);
+    ASSERT_EQ(visBuffer_getLength(buffer), 5);
+
+    VisVisualResult_SetData(&res, fourth,10);
+    ASSERT_EQ(visBuffer_PushBackResult(buffer, &res), 0);
+    ASSERT_EQ(visBuffer_getLength(buffer), 5);
+
+    VisVisualResult_SetData(&res, fifth,10);
+    ASSERT_EQ(visBuffer_PushBackResult(buffer, &res), 0);
+    ASSERT_EQ(visBuffer_getLength(buffer), 5);
+
+    VisVisualResult_SetData(&res, six,10);
+    ASSERT_EQ(visBuffer_PushBackResult(buffer, &res), 0);
+    ASSERT_EQ(visBuffer_getLength(buffer), 5);
+}
+
