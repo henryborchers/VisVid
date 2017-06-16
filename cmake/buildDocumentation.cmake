39992b8f9e11ea00758df43e4ad44dc3ad896401
Henry Borchers <henryborchers@yahoo.com>
2017-06-16 13:59:23 -0500

    Simplified logic on whether or not to build the building docs. Fixed custom_target for building the docs so if trying to install without building the docs, it doesn't error.

--- cmake/buildDocumentation.cmake
+++ cmake/buildDocumentation.cmake
@@ -1,17 +1,23 @@
 find_package(Doxygen)
 
-set(root ${PROJECT_SOURCE_DIR}/lib)
-set(DOC_OUTPUT ${CMAKE_BINARY_DIR}/documentation)
 
-if(VISVID_BUILDDOCS)
-    if (DOXYGEN_FOUND)
+if(VISVID_BUILDDOCS AND DOXYGEN_FOUND)
+        set(root ${PROJECT_SOURCE_DIR}/lib)
+        set(DOC_OUTPUT ${CMAKE_BINARY_DIR}/documentation)
         configure_file(${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
-        add_custom_target(doc ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
-                WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-                COMMENT "Generating documentation" VERBATIM)
+
+
+        add_custom_target(documentation ALL
+                DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/documentation/html
+                )
+        add_custom_command(
+                OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/documentation/html
+                COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
+                COMMENT "Generating documentation"
+
+        )
 
         install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/documentation/html
                 DESTINATION share/doc
                 COMPONENT Documentation)
-    endif(DOXYGEN_FOUND)
-endif(VISVID_BUILDDOCS)
+endif()
