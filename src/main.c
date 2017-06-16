d7de1bc75a82a415b7e502130712c9fd96dc545f
Henry Borchers <henryborchers@yahoo.com>
2017-06-16 14:08:23 -0500

    added a level of indirection for building with NOC or without by creating get_file function()

--- src/main.c
+++ src/main.c
@@ -7,8 +7,34 @@
 #include "runPlayers.h"
 #include "filedialog.h"
 
+static const char *get_file(int argc, char *argv[]);
 
-int main(int argc, char *argv[]){
+
+#ifdef NOC_FILE_DIALOG_IMPLEMENTATION
+
+const char *get_file(int argc, char *argv[]) {
+
+    if (argc == 1) {
+        puts("Opening up a file dialog box");
+        return getFilename();
+    } else if (argc == 2) {
+        return argv[1];
+    } else
+        return NULL;
+}
+
+#else
+const char *get_file(int argc, char *argv[]) {
+    if (argc == 2) {
+            return argv[1];
+        } else {
+            return NULL;
+            }
+}
+#endif
+
+
+int main(int argc, char *argv[]) {
 
     int ret;
 
@@ -18,29 +44,29 @@ int main(int argc, char *argv[]){
     fflush(stdout);
 
     // Check args
-    #ifdef NOC_FILE_DIALOG_IMPLEMENTATION
-    if(argc == 1){
+#ifdef NOC_FILE_DIALOG_IMPLEMENTATION
+    if (argc == 1) {
         puts("Opening up a file dialog box");
-        filename = getFilename();
-        if(!filename){
+        filename = get_file(argc, argv);
+        if (!filename) {
             return 0;
         }
     }
-    #else
+#else
     if(argc != 2){
         fprintf(stderr, "You need a file name to continue\n");
         return 1;
     }
-    #endif
+#endif
 
-    if(argc == 2){
+    if (argc == 2) {
         filename = argv[1];
     }
-    if((ret = playVideoVis(filename)) !=0){
+    if ((ret = playVideoVis(filename)) != 0) {
         fprintf(stderr, "%s exited with error code: %d.\n", argv[0], ret);
         return ret;
     };
-    return  0;
+    return 0;
 
 }
 
