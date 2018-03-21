e87988e8209f92c58b987f8c212bc66f6bc462c3
henryborchers <henryborchers@yahoo.com>
2018-03-21 18:37:27 -0500

    init commit

---
+++ build.cmake
@@ -0,0 +1,17 @@
+set(CTEST_SOURCE_DIRECTORY .)
+set(CTEST_BINARY_DIRECTORY  ./binary)
+
+set(ENV{CXXFLAGS} "--coverage")
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+
+set(CTEST_COVERAGE_COMMAND "gcov")
+set(CTEST_MEMORYCHECK_COMMAND "/usr/bin/valgrind")
+
+ctest_start("Continuous")
+#ctest_update()
+ctest_configure()
+ctest_build()
+ctest_test()
+ctest_coverage()
+ctest_memcheck()
+#ctest_submit()
