efd7587e17712f07f87b08b1728207b0c2f947f2
henryborchers <henryborchers@yahoo.com>
2018-03-21 19:43:13 -0500

    use make instead of Ninja

--- build.cmake
+++ build.cmake
@@ -2,13 +2,13 @@ set(CTEST_SOURCE_DIRECTORY .)
 set(CTEST_BINARY_DIRECTORY  ./build)
 
 set(ENV{CXXFLAGS} "--coverage")
-set(CTEST_CMAKE_GENERATOR "Ninja")
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 
 set(CTEST_COVERAGE_COMMAND "gcov")
 set(CTEST_MEMORYCHECK_COMMAND "/usr/bin/valgrind")
 
 ctest_start("Continuous")
-#ctest_update()
+ctest_update()
 ctest_configure()
 ctest_build()
 ctest_test()
