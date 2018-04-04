e107c55339a6c1223d868005a6fc29f4f0f133da
henryborchers <henryborchers@yahoo.com>
2018-04-04 14:30:01 -0500

    rewrote visView Functions on a full buffer as a BDD image

--- tests/test-visView.cpp
+++ tests/test-visView.cpp
@@ -65,59 +65,70 @@ TEST_CASE("visView Functions on an Empty Buffer"){
     CHECK(nullptr == pvid);
 
 }
+SCENARIO("visView full buffer"){
+    GIVEN("visView Functions on a full buffer of empty data"){
+        visView *pvid = nullptr;
+        visBuffer *buffer = nullptr;
 
-TEST_CASE("visView Functions on a full buffer"){
-    visView *pvid = nullptr;
-    visBuffer *buffer = nullptr;
+        buffer= VisBuffer_Create2(10, 0);
+        CHECK(buffer != nullptr);
 
-    buffer= VisBuffer_Create2(10, 0);
-    CHECK(buffer != nullptr);
+        pvid = VisView_Create(2, 480);
+        CHECK(pvid != nullptr);
 
-    pvid = VisView_Create(2, 480);
-    CHECK(pvid != nullptr);
 
-    visBuffer_PushBackResult(buffer, nullptr);
-    visBuffer_PushBackResult(buffer, nullptr);
+        WHEN("The buffer has no data"){
+            visBuffer_PushBackResult(buffer, nullptr);
+            visBuffer_PushBackResult(buffer, nullptr);
 
-    SECTION("Update view"){
-        CHECK(visView_Update(pvid, buffer) == 0);
-        for(int x = 0; x<pvid->width; x++){
-            for(int y = 0; y<pvid->height; y++){
-                CHECK(pvid->data[x + pvid->width * y] == 0);
+            CHECK(visView_Update(pvid, buffer) == 0);
+
+            THEN("All the data in the view is completely black"){
+                for(int x = 0; x<pvid->width; x++){
+                    for(int y = 0; y<pvid->height; y++){
+                        CHECK(pvid->data[x + pvid->width * y] == 0);
+                    }
+
+                };
             }
 
-        };
-    }
+            AND_WHEN("The view is converted into a RGB image"){
 
-    SECTION("Create_visViewRGBA"){
-        const int ALPHA_SHIFT = 0;
-        const int BLUE_SHIFT  = 1;
-        const int GREEN_SHIFT = 2;
-        const int RED_SHIFT   = 3;
-
-        CHECK(visView_Update(pvid, buffer) == 0);
-        visImageRGB imageRGB;
+                visImageRGB imageRGB;
 
 
-        CHECK(visImageRGB_Alloc(&imageRGB, pvid->width, pvid->height) == 0);
-        CHECK(visViewRGB_GenerateRGBA(&imageRGB, pvid, visViewRGBA_value2BW) == 0);
+                CHECK(visImageRGB_Alloc(&imageRGB, pvid->width, pvid->height) == 0);
+                CHECK(visViewRGB_GenerateRGBA(&imageRGB, pvid, visViewRGBA_value2BW) == 0);
 
-        for (int y = 0; y < imageRGB.height; ++y) {
-            for (int x = 0; x < imageRGB.width; ++x) {
-                int offset = (y * imageRGB.pitch) + (x * sizeof(uint8_t) * 4);
-                CHECK(imageRGB.plane[offset + ALPHA_SHIFT] == 0);
-                CHECK(imageRGB.plane[offset + RED_SHIFT] == 0);
-                CHECK(imageRGB.plane[offset + BLUE_SHIFT] == 0);
-                CHECK(imageRGB.plane[offset + GREEN_SHIFT] == 0);
+                THEN("Resulting image is completely black"){
+
+                    const int ALPHA_SHIFT = 0;
+                    const int BLUE_SHIFT  = 1;
+                    const int GREEN_SHIFT = 2;
+                    const int RED_SHIFT   = 3;
+
+                    for (int y = 0; y < imageRGB.height; ++y) {
+                        for (int x = 0; x < imageRGB.width; ++x) {
+                            int offset = (y * imageRGB.pitch) + (x * sizeof(uint8_t) * 4);
+                            CHECK(imageRGB.plane[offset + ALPHA_SHIFT] == 0);
+                            CHECK(imageRGB.plane[offset + RED_SHIFT] == 0);
+                            CHECK(imageRGB.plane[offset + BLUE_SHIFT] == 0);
+                            CHECK(imageRGB.plane[offset + GREEN_SHIFT] == 0);
+                        }
+                    }
+                }
+
+                visImageRGB_FreeData(&imageRGB);
             }
+
         }
-        visImageRGB_FreeData(&imageRGB);
+
+
+        VisView_Destroy(&pvid);
+        CHECK(pvid == nullptr);
+
+        VisBuffer_Destroy(&buffer);
+        CHECK(buffer == nullptr);
+
     }
-
-    VisView_Destroy(&pvid);
-    CHECK(pvid == nullptr);
-
-    VisBuffer_Destroy(&buffer);
-    CHECK(buffer == nullptr);
-
 }
