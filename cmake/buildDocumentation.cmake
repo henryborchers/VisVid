e37c7fcbf208f62d4682863033ef565afb4e3863
Henry Borchers <henryborchers@yahoo.com>
2018-11-29 21:50:20 -0800

    sphinx only runs docs on the public headers

--- cmake/buildDocumentation.cmake
+++ cmake/buildDocumentation.cmake
@@ -2,7 +2,8 @@ find_package(Doxygen)
 
 
 if(VISVID_BUILDDOCS AND DOXYGEN_FOUND)
-        doxygen_add_docs(documentation ${PROJECT_SOURCE_DIR}/lib
+        doxygen_add_docs(documentation visvid
+                WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/src/visvid/include
                 COMMENT "Generated html pages"
                 DOXYGEN_HAVE_DOT YES
                 )
