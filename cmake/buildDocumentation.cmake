6f61a4aeebe683b1a0763d5c5c53c91d48d105c2
Henry Borchers <henryborchers@yahoo.com>
2018-11-29 21:54:06 -0800

    building docs becomes an optional target if doxygen is located

--- cmake/buildDocumentation.cmake
+++ cmake/buildDocumentation.cmake
@@ -1,28 +1,10 @@
 find_package(Doxygen)
 
 
-if(VISVID_BUILDDOCS AND DOXYGEN_FOUND)
+if(DOXYGEN_FOUND)
         doxygen_add_docs(documentation visvid
                 WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/src/visvid/include
                 COMMENT "Generated html pages"
                 DOXYGEN_HAVE_DOT YES
                 )
-#        set(root ${PROJECT_SOURCE_DIR}/visvid)
-#        set(DOC_OUTPUT ${CMAKE_BINARY_DIR}/documentation)
-#        configure_file(${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
-
-
-#        add_custom_target(documentation ALL
-#                DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/documentation/html
-#                )
-#        add_custom_command(
-#                OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/documentation/html
-#                COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
-#                COMMENT "Generating documentation"
-#
-#        )
-
-#        install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/documentation/html
-#                DESTINATION share/doc
-#                COMPONENT Documentation)
 endif()
