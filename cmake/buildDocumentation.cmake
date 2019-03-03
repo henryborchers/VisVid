eb73f77927ba8ee28a5a0ad0499f2ddbbf34b143
henryborchers <henryborchers@yahoo.com>
2019-03-02 23:24:08 -0600

    build documentation in docs subdir

--- cmake/buildDocumentation.cmake
+++ cmake/buildDocumentation.cmake
@@ -4,6 +4,7 @@ find_package(Doxygen)
 if(DOXYGEN_FOUND)
         set(DOXYGEN_EXAMPLE_PATH "${PROJECT_SOURCE_DIR}/src/applications")
         set(DOXYGEN_EXCLUDE "${PROJECT_SOURCE_DIR}/src/applications/createVisuals/thirdpartylibs")
+        set(DOXYGEN_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/docs/)
         doxygen_add_docs(documentation visvid ${PROJECT_SOURCE_DIR}/docs/mainpage.dox
                 WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/src/visvid/include
                 COMMENT "Generating ${PROJECT_NAME} api documentation"
