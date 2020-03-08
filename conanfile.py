41d4071c36b946174acfa69809e0e9eb8ba3cfc9
henry <henryborchers@yahoo.com>
2020-03-07 22:53:09 -0600

    updated conanfile to run with latest version of SDL2

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,14 @@
+import os
+
 from conans import ConanFile, CMake
 
 class VisvidConan(ConanFile):
     requires = [
         "ffmpeg/4.2.1@bincrafters/stable",
-        "sdl2/2.0.10@bincrafters/stable"
+        "sdl2/2.0.10@bincrafters/stable",
     ]
+    settings = "os", "arch", "compiler", "build_type"
+
     generators = ["cmake_paths"]
     default_options = {
         "ffmpeg:shared": True,
@@ -13,3 +17,16 @@ class VisvidConan(ConanFile):
     def imports(self):
         self.copy("*.dll", dst="bin", src="bin") # From bin to bin
         self.copy("*.dylib*", dst="bin", src="lib") # From lib to bin
+
+    def configure(self):
+        if self.settings.os == "Linux":
+            self.options["ffmpeg"].vorbis = False
+
+    def build(self):
+        cmake = CMake(self)
+        cmake_toolchain_file = os.path.join(self.build_folder, "conan_paths.cmake")
+        assert os.path.exists(cmake_toolchain_file)
+        cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = cmake_toolchain_file
+        cmake.configure()
+        cmake.build()
+
